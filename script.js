504491bea51502a64bc1f282c59d5ba390410c06
Rubenildo Marques <82522946+Marques-Rubenildo@users.noreply.github.com>
2021-05-03 20:17:03 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function carregar() {
-    var msg = window.document.getElementById("msg")
+    var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // bom dia
+        img.src = 'manha.png'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'tarde.png'
+    } else {
+        //boa noite
+        img.src = 'noite'
+    }
 }
